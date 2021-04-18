99723be9b3f9c734e0e36672219c55c5f1cb5be1
Stewart Jester <jesterstewart@gmail.com>
2021-04-18 16:13:56 -0700

    more stuff

--- App.js
+++ App.js
@@ -7,60 +7,198 @@ export default function App() {
   const [bloodlust, setBloodlust] = useState(0)
   const [customers, setCutomers] = useState(0)
   const [money, setMoney] = useState(0)
+  const [superFood, setSuperFood] = useState(false)
+  const [superFoodCount, setSuperFoodCount] = useState(0)
+  const [inspector, setInspector] = useState(false)
+  const [arrested, setArrested] = useState(false)
+  const [risk, setRisk] = useState(0)
+  
+  const reset = () => {
+    setBloodlust(0)
+    setCutomers(0)
+    setMoney(0)
+    setSuperFoodCount(0)
+    setArrested(false)
+    setInspector(false)
+    setRisk(0)
+  }
+  
+  const randomInteger = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+  
+  const inspectorVisit = () => {
+   let riskTaken = 20
+   if (risk > 5 && risk < 10) {
+     riskTaken = 15
+   } else if (risk > 10) {
+     riskTaken = 10
+   }
+   let number = randomInteger(1, riskTaken)
+   if (number === 5) {
+     setInspector(true)
+   }
+  console.log(number)
+  }
+  
+  const bribe = () => {
+    setInspector(false)
+    if (money >= 20) {
+      setMoney(money - 20)
+      setInspector(false)
+      setRisk(risk - 5)
+    } else if (money < 20) {
+      setArrested(true)
+    }
+    console.log(arrested)
+  }
   
   const feedHim = () => {
+    let number = randomInteger(5, 20)
     if (customers === 0) {
       alert('the store is empty')
     } else if (money < 5) {
-      alert('You are bankrupt. Game Over')
-      setMoney(0);
-      setCutomers(0)
-      setBloodlust(0)
+      alert('Not Enough Money')
     }
     
-    else if (bloodlust < 10) {
+    else if (bloodlust < number) {
       setBloodlust(0)
       setCutomers(customers - 1)
       setMoney(money - 5)
+      inspectorVisit()
+      setRisk(risk + 1)
     }
     else {
-      setBloodlust(bloodlust - 10)
+      setBloodlust(bloodlust - number)
       setCutomers(customers - 1)
       setMoney(money - 5)
+      inspectorVisit()
+      setRisk(risk + 1)
+    }
+    
+  }
+  
+  const buySuperFood = () => {
+    if (money >= 20) {
+      setSuperFood(true);
+      setBloodlust(bloodlust - 10)
+      setSuperFoodCount(superFoodCount + 3)
+      setMoney(money - 20)
+      inspectorVisit()
+      setRisk(risk + 1)
+    } else {
+      alert('not enough money')
     }
     
   }
   
   const letInCustomer = () => {
-    setCutomers(customers + 1);
-    setBloodlust(bloodlust + 5);
+    setCutomers(customers + 1)
+    setRisk(risk + 1)
+//     setBloodlust(bloodlust + 5);
+    
   }
   
   const makeSale = () => {
+    let cash = randomInteger(5, 20)
+    let hunger = randomInteger(5, 20)
     if (customers === 0) {
       alert('the store is empty')
-    } else if (money >= 990 && bloodlust < 95) {
-      setMoney(money + 10);
-      setBloodlust(bloodlust + 5)
-      alert('You made rent without getting eaten! You win!')
-      setMoney(0);
-      setCutomers(0)
-      setBloodlust(0)
-    } else if (bloodlust >= 95) {
-      alert('Uh Oh! Your plant got to hungry and ate you!')
-      setMoney(0);
-      setCutomers(0)
-      setBloodlust(0)
     }
     
-      else {
-      setMoney(money + 10);
+      else if (superFoodCount > 0) {
+      setMoney(money + cash);
       setCutomers(customers - 1)
-      setBloodlust(bloodlust + 5)
-    } 
+      setSuperFoodCount(superFoodCount - 1)
+      inspectorVisit()
+      setBloodlust(bloodlust + (hunger % 2))
+      setRisk(risk + 1)
+    } else {
+      setMoney(money + cash);
+      setCutomers(customers - 1)
+      setBloodlust(bloodlust + hunger)
+      inspectorVisit()
+      setRisk(risk + 1)
+    }
   }
   
-  return (
+  if (bloodlust >= 100) {
+    return (
+      <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Your plant got too hungry and ate you!</Text>
+    
+      <Button
+        title="Try Again"
+        onPress={reset}
+        />
+      
+    </View>
+    )
+  } else if (money >= 100 && bloodlust < 100) {
+    return (
+      <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>You made rent without getting eaten! You win!</Text>
+    
+      <Button
+        title="Try Again"
+        onPress={reset}
+        />
+      
+    </View>
+    )
+  } else if (inspector === true) {
+    return(
+      <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>A detective comes snooping around...</Text>
+    
+      <Button
+        title="Bribe Him - $20"
+        onPress={bribe}
+        />
+      
+    </View>
+    )
+  } else if (arrested === true) {
+    return(
+      <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>You didn't have enough money. You are under arrest</Text>
+    
+      <Button
+        title="Try Again"
+        onPress={reset}
+        />
+      
+    </View>
+    )
+  } 
+  
+  
+  else {
+    return (
     <View
       style={{
         flex: 1,
@@ -71,9 +209,15 @@ export default function App() {
       <Text>Customers in store: {customers}</Text>
       <Text>Money: {money}</Text>
       <Text>Your Plant's Bloodlust is {bloodlust}</Text>
+      <Text>Superfood: {superFoodCount}</Text>
+        <Text>Suspicion: {risk}</Text>
       <Button
-        title="Feed Me"
+        title="Feed Him a Customer"
         onPress={feedHim}
+        />
+      <Button
+        title="Buy Superfood"
+        onPress={buySuperFood}
         />
       <Button
         title="Let in a customer"
@@ -85,5 +229,9 @@ export default function App() {
         />
       
     </View>
-  );
+  )
+  }
+  
+  
+  
 }
